4298c0228ef43fb57c016d1d15ff3b152bf770dd
Rijk van Putten <rijk@rijkvp.nl>
2025-01-27 11:59:31 +0100

    Seperate dev/prod configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+import wasmPack from 'vite-plugin-wasm-pack';
 
-export default defineConfig({
-    plugins: [
-        sveltekit(),
-        wasm(),
-        topLevelAwait(),
-    ],
-    server: {
-        fs: {
-            allow: ["./tetris-ai"]
-        }
+export default defineConfig(({ mode }) => {
+    let wasmPlugins: PluginOption[] = [];
+    if (mode === 'development') {
+        wasmPlugins = [wasmPack('./tetris-ai')];
+    } else if (mode === 'production') {
+        wasmPlugins = [wasm(), topLevelAwait()];
     }
+
+    return {
+        plugins: [
+            sveltekit(),
+            ...wasmPlugins,
+        ],
+        server: {
+            fs: {
+                allow: ["./tetris-ai"]
+            }
+        }
+    };
 });
