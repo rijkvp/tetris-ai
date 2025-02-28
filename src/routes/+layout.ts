7c0544c7867e1af96d6e27ace8dcd979d3cf853c
Rijk van Putten <rijk@rijkvp.nl>
2025-02-28 17:03:53 +0100

    Route based on language

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,17 +1,28 @@
 export const ssr = false;
 export const prerender = true;
 
-import { loadTranslations } from '$lib/translations';
-import type { LayoutLoad } from './$types';
+import { loadTranslations } from "$lib/translations";
+import type { LayoutLoad } from "./$types";
 import init from "tetris-ai";
+import { redirect } from "@sveltejs/kit";
 
 export const load: LayoutLoad = async ({ url }) => {
     await init();
-    console.info('Tetris AI initialized');
+    console.info("Tetris AI initialized");
 
     const { pathname } = url;
-
-    await loadTranslations('en', pathname);
+    if (pathname === "/") {
+        if (navigator.language.startsWith("nl")) {
+            return redirect(302, "/nl");
+        } else {
+            return redirect(302, "/en");
+        }
+    }
+    if (pathname.startsWith("/en")) {
+        await loadTranslations("en", "");
+    } else if (pathname.startsWith("/nl")) {
+        await loadTranslations("nl", "");
+    }
 
     return {};
 };
