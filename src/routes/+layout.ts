16bff0e700c9c34982904c4ead8ed2adef466872
Rijk van Putten <rijk@rijkvp.nl>
2025-03-13 19:53:52 +0100

    Try to fix routing

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -5,12 +5,16 @@ import { loadTranslations } from "$lib/translations";
 import type { LayoutLoad } from "./$types";
 import init from "tetris-ai";
 import { redirect } from "@sveltejs/kit";
+import { base } from "$app/paths";
 
 export const load: LayoutLoad = async ({ url }) => {
     await init();
     console.info("Tetris AI initialized");
 
-    const { pathname } = url;
+    let { pathname } = url;
+    if (pathname.startsWith(base)) {
+        pathname = pathname.substring(base.length);
+    }
     if (pathname.startsWith("/en")) {
         await loadTranslations("en", "");
     } else if (pathname.startsWith("/nl")) {
