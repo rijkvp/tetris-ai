82a96d97b81c9362616f9ec3b3d8409a80d45e4d
Rijk van Putten <rijk@rijkvp.nl>
2025-03-21 15:36:34 +0100

    Update deps

--- src/lib/theme.svelte.ts
+++ src/lib/theme.svelte.ts
@@ -1,4 +1,3 @@
-
 let prefersDark = $state(
     window ?
         window.matchMedia('(prefers-color-scheme: dark)').matches : false,
@@ -6,6 +5,7 @@ let prefersDark = $state(
 
 let modeName = $derived(() => prefersDark ? "dark" : "light");
 
+// TODO: this does not work
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
     prefersDark = e.matches;
 });
