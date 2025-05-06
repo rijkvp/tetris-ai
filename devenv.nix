7d9cfc35d503ba93b769b4fec5f1f3ebccee3465
Rijk van Putten <rijk@rijkvp.nl>
2025-05-06 11:02:14 +0200

    Switch to pnpm

--- devenv.nix
+++ devenv.nix
@@ -1,17 +1,16 @@
 {
   pkgs,
-  lib,
   ...
 }:
 {
-  packages = [
-    pkgs.cargo-edit
-    pkgs.python3
-    pkgs.python3Packages.numpy
-    pkgs.cargo-flamegraph
-    pkgs.wasm-pack
-    pkgs.nodejs
-    pkgs.yarn
+  packages = with pkgs; [
+    cargo-edit
+    python3
+    python3Packages.numpy
+    cargo-flamegraph
+    wasm-pack
+    nodejs
+    pnpm
   ];
 
   languages.rust = {
@@ -44,28 +43,10 @@
     };
   };
 
-  env.LD_LIBRARY_PATH =
-    with pkgs;
-    lib.makeLibraryPath [
-      python3
-      python3Packages.numpy
-    ];
-
-  scripts.deploy.exec = ''
-    #!/bin/sh
-    if [ -z "$1" ]; then
-      echo "usage: $0 <deploy_dir>"
-      exit 1
-    fi
-    npm run build
-    cd "$1"
-    find . -mindepth 1 -maxdepth 1 -not -name '.git' -exec rm -rf {} +
-    cp -a "$DEVENV_ROOT/build/." .
-    touch .nojekyll
-  '';
-
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
+  # env.LD_LIBRARY_PATH =
+  #   with pkgs;
+  #   lib.makeLibraryPath [
+  #     python3
+  #     python3Packages.numpy
+  #   ];
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig, type PluginOption } from 'vite';
+import { defineConfig } from 'vite';
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
-import wasmPack from 'vite-plugin-wasm-pack';
 
-export default defineConfig(({ mode }) => {
-    let wasmPlugins: PluginOption[] = [];
-    if (mode === 'development') {
-        wasmPlugins = [wasmPack('./tetris-ai')];
-    } else if (mode === 'production') {
-        wasmPlugins = [wasm(), topLevelAwait()];
-    }
-
+export default defineConfig(() => {
     return {
         plugins: [
             sveltekit(),
-            ...wasmPlugins,
+            wasm(),
+            topLevelAwait()
         ],
         server: {
             fs: {
