2fe62641812a4b811e47120d3cb97a549d94bd0e
Rijk van Putten <rijk@rijkvp.nl>
2025-05-22 17:42:30 +0200

    Fixed routing

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -20,6 +20,12 @@ export const load: LayoutLoad = async ({ url }) => {
     } else if (appPath.startsWith("/nl")) {
         document.documentElement.lang = 'nl';
         await loadTranslations("nl", "");
+    } else if (appPath === "/") {
+        if (navigator.language.startsWith("nl")) {
+            return redirect(302, `${base}/nl`);
+        } else {
+            return redirect(302, `${base}/en`);
+        }
     } else {
         const lang = navigator.language.startsWith("nl") ? "nl" : "en";
         return redirect(302, `${base}/${lang}${pathname}`);
